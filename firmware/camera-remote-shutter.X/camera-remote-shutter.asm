9f4aa61bcd217a7fd646745e2eefb198b1006d76
Alessandro Fraschetti <gos95@gommagomma.net>
2018-06-13 16:04:50 +0200

    add license notice on asm file

--- firmware/camera-remote-shutter.X/camera-remote-shutter.asm
+++ firmware/camera-remote-shutter.X/camera-remote-shutter.asm
@@ -6,37 +6,52 @@
 ; \    \_\  (  <_> )        \  /    //       \
 ;  \______  /\____/_______  / /____//______  /
 ;         \/              \/               \/
-; Copyright (c) 2014 by Alessandro Fraschetti.
-; All Rights Reserved.
+; Copyright (c) 2014 by Alessandro Fraschetti
 ;
 ; Description: Camera Remote Shutter/Focus Trigger Controller
 ; Target.....: Microchip PIC 12F683 Microcontroller
 ; Compiler...: Microchip Assembler (MPASM)
 ; Note.......: tested on Canon EOS400D
+;
+; MIT License
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
 ;=============================================================================
 
         processor   12f683
         #include    <p12f683.inc>
         __CONFIG    _CP_OFF & _CPD_OFF & _BOREN_OFF & _WDT_OFF & _MCLRE_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT
-					; _CP_[ON/OFF]    : code protect program memory enable/disable
+                    ; _CP_[ON/OFF]    : code protect program memory enable/disable
                     ; _CPD_[ON/OFF]   : code protect data memory enable/disable
-					; _BODEN_[ON/OFF] : Brown-Out Reset enable/disable
-					; _WDT_[ON/OFF]   : watchdog timer enable/disable
-					; _MCLRE_[ON/OFF] : MCLR pin function  digital IO/MCLR
-					; _PWRTE_[ON/OFF] : power-up timer enable/disable
+                    ; _BODEN_[ON/OFF] : Brown-Out Reset enable/disable
+                    ; _WDT_[ON/OFF]   : watchdog timer enable/disable
+                    ; _MCLRE_[ON/OFF] : MCLR pin function  digital IO/MCLR
+                    ; _PWRTE_[ON/OFF] : power-up timer enable/disable
 
 ;=============================================================================
 ;  File register use
 ;=============================================================================
-		cblock		h'20'
-			w_temp						; variable used for context saving
-			status_temp					; variable used for context saving
+        cblock      h'20'
+            w_temp                      ; variable used for context saving
+            status_temp                 ; variable used for context saving
             pclath_temp                 ; variable used for context saving
-			d1, d2, d3					; delay routine vars
+            d1, d2, d3                  ; delay routine vars
 
             controllerStatus            ; controller status register
             switchStatus                ; switches status register
-		endc
+        endc
 
 
 ;=============================================================================
@@ -56,28 +71,28 @@ FOCUS       equ     0x01
 ;  Start of code
 ;=============================================================================
 ;start
-		org			h'0000'				; processor reset vector
-		goto		main				; jump to the main routine
+        org         h'0000'             ; processor reset vector
+        goto        main                ; jump to the main routine
 
-		org			h'0004'				; interrupt vector location
-		movwf		w_temp				; save off current W register contents
-		swapf		STATUS, W			; move status register into W register
-		movwf		status_temp			; save off contents of STATUS register
+        org         h'0004'             ; interrupt vector location
+        movwf       w_temp              ; save off current W register contents
+        swapf       STATUS, W           ; move status register into W register
+        movwf       status_temp         ; save off contents of STATUS register
         swapf       PCLATH, W           ; move pclath register into W register
         movwf       pclath_temp         ; save off contents of PCLATH register
 
         ; isr code can go here or be located as a call subroutine elsewhere
-        clrf		STATUS				; select Bank0
+        clrf        STATUS              ; select Bank0
         movf        GPIO, W
         bcf         INTCON, GPIF        ; clear interrupt flag
 
         swapf       pclath_temp, W      ; retrieve copy of PCLATH register
         movwf       PCLATH              ; restore pre-isr PCLATH register contents
-		swapf		status_temp, W		; retrieve copy of STATUS register
-		movwf		STATUS				; restore pre-isr STATUS register contents
-		swapf		w_temp, F
-		swapf		w_temp, W			; restore pre-isr W register contents
-		retfie							; return from interrupt
+        swapf       status_temp, W      ; retrieve copy of STATUS register
+        movwf       STATUS              ; restore pre-isr STATUS register contents
+        swapf       w_temp, F
+        swapf       w_temp, W           ; restore pre-isr W register contents
+        retfie                          ; return from interrupt
 
 
 ;=============================================================================
@@ -86,16 +101,16 @@ FOCUS       equ     0x01
 ;=============================================================================
 init_osc:
 
-		errorlevel	-302
+        errorlevel  -302
 
-        bsf			STATUS, RP0				; select Bank1
+        bsf         STATUS, RP0             ; select Bank1
         movlw       b'00000111'             ; set the clock speed to 31KHz
         movwf       OSCCON
-        bcf			STATUS, RP0				; select Bank0
+        bcf         STATUS, RP0             ; select Bank0
 
-		errorlevel  +302
+        errorlevel  +302
 
-  		return
+        return
 
 
 ;=============================================================================
@@ -104,23 +119,23 @@ init_osc:
 ;=============================================================================
 init_ports:
 
-		errorlevel	-302
+        errorlevel  -302
 
-        bsf			STATUS, RP0				; select Bank1
+        bsf         STATUS, RP0             ; select Bank1
         clrf        GPIO                    ; init GPIO
 
         movlw       0x07                    ; turn comparators off
         movwf       CMCON0
         clrf        ANSEL                   ; make all ports as digital I/O
 
-        movlw		b'111000'				; PORT input/output
+        movlw       b'111000'               ; PORT input/output
         movwf       TRISIO
-        
-        bcf			STATUS, RP0				; select Bank0
 
-		errorlevel  +302
+        bcf         STATUS, RP0             ; select Bank0
 
-  		return
+        errorlevel  +302
+
+        return
 
 
 ;=============================================================================
@@ -129,17 +144,17 @@ init_ports:
 ;=============================================================================
 init_interrupt:
 
-		errorlevel	-302
+        errorlevel  -302
 
-        bsf			STATUS, RP0				; select Bank1
+        bsf         STATUS, RP0             ; select Bank1
         bsf         IOC, INT_ON_SW          ; enable int-on-change
         movlw       b'10001000'             ; enable port change and global int
         movwf       INTCON
-        bcf			STATUS, RP0				; select Bank0
+        bcf         STATUS, RP0             ; select Bank0
 
-		errorlevel  +302
+        errorlevel  +302
 
-  		return
+        return
 
 
 ;=============================================================================
@@ -164,8 +179,8 @@ delayLoop
 ;  main routine
 ;=============================================================================
 main
-        call 		init_osc
-		call 		init_ports
+        call        init_osc
+        call        init_ports
         call        init_interrupt
 
         clrf        controllerStatus
